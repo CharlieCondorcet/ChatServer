5dc282d90121b704b2717ca9e3bddab20a34d786
Charlie Condorcet <ccm059@alumnos.ucn.cl>
2019-09-22 17:16:49 -0300

    agregando clases bases para el proyecto

---
+++ src/main/java/ChatThread.java
@@ -0,0 +1,14 @@
+/*
+ *  Copyright (c) 2019. Charlie Condorcet http://www.charliec.cl .
+ *
+ *  All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Intellij IDEA
+ * which accompanies this distribution, and is available at
+ * https://resources.jetbrains.com/storage/products/appcode/docs/AppCode_Classroom_EULA.pdf .
+ *
+ */
+
+public class ChatThread {
+
+
+}
