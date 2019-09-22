de05171d45f248937dd5d66879b4f4478a29edf8
Charlie Condorcet <ccm059@alumnos.ucn.cl>
2019-09-22 01:33:32 -0300

    agregando el proyecto completo

---
+++ src/main/java/ChatServer.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class ChatServer {
+
+    public static void main(final String[] args) throws IOException {
+
+
+        System.out.println("imprimiendo primer dato");
+    }
+
+}
