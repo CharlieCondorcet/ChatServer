6a78cdc59bf855eb3b90cfc08846daed9b5149b7
Charlie Condorcet <ccm059@alumnos.ucn.cl>
2019-09-23 17:54:43 -0300

    actualizando cambios para manejo correcto de librerias org.slf4j

--- src/main/java/ChatServer.java
+++ src/main/java/ChatServer.java
@@ -11,50 +11,65 @@
 
 
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.text.Normalizer;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 
 public class ChatServer {
 
+    /**
+     * The Logger
+     */
+    private static final Logger log = LoggerFactory.getLogger(ChatServer.class);
 
-    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger( ChatServer.class );
-
-
+    /**
+     * The Port
+     */
     private static final int PORT = 9000;
 
-    private static final ArrayList<ChatThread> clientesActivos = new ArrayList<>();
+    private static final ArrayList<ChatMessage> MessagesInChatRoom = new ArrayList<>();
+    private static int contMessages = 0;
 
-    private static final List<ChatMessage> MessagesInChatRoom = new ArrayList<>();
-
-
-
+    /**
+     * The Ppal.
+     */
     public static void main(final String[] args) throws IOException {
-
 
         log.debug("Starting the Main ..");
 
+        // The Server Socket
         final ServerSocket serverSocket = new ServerSocket(PORT);
 
+        // serverSocket.setReuseAddress(true);
         log.debug("Server started in port {}, waiting for connections ..", PORT);
 
+        // Forever serve.
         while (true) {
 
+            // One socket by request (try with resources).
             try {
 
                 final Socket socket = serverSocket.accept();
 
-                ChatThread newClient = new ChatThread (socket, PORT, MessagesInChatRoom);
-                newClient.start();
+                // The remote connection address.
+                final InetAddress address = socket.getInetAddress();
 
-                clientesActivos.add(newClient);
+                log.debug("========================================================================================");
+                log.debug("Connection from {} in port {}.", address.getHostAddress(), socket.getPort());
 
-                MessagesInChatRoom.add(newClient.SaveNewMessage());
-
+                processConnection(socket);
 
             } catch (IOException e) {
                 log.error("Error", e);
@@ -63,9 +78,116 @@ public class ChatServer {
 
         }
 
+    }
 
+    /**
+     * Process the connection.
+     *
+     * @param socket to use as source of data.
+     */
+    private static void processConnection(final Socket socket) throws IOException {
+
+        // Reading the inputstream
+        final List<String> lines = readInputStreamByLines(socket);
+
+        final String request = lines.get(0);
+        log.debug("Request: {}", request);
+
+        final PrintWriter pw = new PrintWriter(socket.getOutputStream());
+        pw.println("HTTP/1.1 200 OK");
+        pw.println("Server: DSM v0.0.1");
+        pw.println("Date: " + new Date());
+        pw.println("Content-Type: text/html; charset=UTF-8");
+        // pw.println("Content-Type: text/plain; charset=UTF-8");
+        pw.println();
+        //pw.println("<html><body>The Date: <strong>" + new Date() + "</strong><body></html>");
+
+
+        pw.println("<html><body> <h2><font color=\"red\"> CHAT ROOM </font> </h2>  <p>allMessages:</p> ");
+        pw.println("<textarea  readonly cols=\"160\" rows=\"20\" > ");
+
+        for (int i = 0; i < MessagesInChatRoom.size(); i++) {
+            pw.println(MessagesInChatRoom.get(i).PrintCompleteMessage());
+        }
+
+        pw.println("</textarea> <form method=\"POST\"> ");
+        pw.println("<br> <hr> <br> Enter you Name:<input type=\"text\" name=\"myUserName\" style=\"margin-right: 20px\"> ");
+        pw.println("Enter you Message:<input type=\"text\" name=\"myMessage\" size=\"50\" style=\"margin-right: 20px\"> ");
+        pw.println("<input type=\"submit\" name=\"myButton\" value=\"Send to Everybody\"> ");
+        pw.println("</form> <body></html>");
+
+        pw.flush();
+
+        log.debug("Process ended.");
+
+    }
+
+    /**
+     * Read all the input stream.
+     *
+     * @param socket to use to read.
+     * @return all the string readed.
+     */
+    private static List<String> readInputStreamByLines(final Socket socket) throws IOException {
+
+        final InputStream is = socket.getInputStream();
+
+        // The list of string readed from inputstream.
+        final List<String> lines = new ArrayList<>();
+
+        // The Scanner
+        final Scanner s = new Scanner(is).useDelimiter("\\A");
+        log.debug("Reading the Inputstream ..");
+
+        while (true && s.hasNext()) {
+
+            final String line = s.nextLine();
+            // log.debug("Line: [{}].", line);
+
+            if (line.length() == 0) {
+
+                if (lines.get(0).equals("POST / HTTP/1.1")) {
+                    String textByUser = s.nextLine();
+                    FormatForMessage(textByUser);
+                }
+
+                break;
+            } else {
+                lines.add(line);
+            }
+        }
+        // String result = s.hasNext() ? s.next() : "";
+
+        // final List<String> lines = IOUtils.readLines(is, StandardCharsets.UTF_8);
+        return lines;
+
+    }
+
+
+    public static void FormatForMessage(String completeLine) {
+
+        /**
+         variables para guardar el nombre del nombre del usuario y mensaje del usuario respectivamente.
+         */
+        String nameUser = "", messageUser = "";
+
+        nameUser += completeLine.substring(11, completeLine.indexOf("&myMessage="));
+        messageUser += completeLine.substring(completeLine.indexOf("&myMessage=") + 11, completeLine.indexOf("&myButton="));
+
+        String withoutAccent = Normalizer.normalize(nameUser, Normalizer.Form.NFD);
+        nameUser = withoutAccent.replaceAll("[^a-zA-Z ]", " ");
+
+        withoutAccent = Normalizer.normalize(messageUser, Normalizer.Form.NFD);
+        messageUser = withoutAccent.replaceAll("[^a-zA-Z ]", " ");
+
+        LocalDateTime timeStamp = LocalDateTime.now();
+
+        ChatMessage newMessage = new ChatMessage(nameUser, messageUser, timeStamp);
+        MessagesInChatRoom.add(newMessage);
+        contMessages++;
 
 
     }
+
 
 }
